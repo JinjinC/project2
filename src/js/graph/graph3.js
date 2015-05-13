8616e075499db716ab37ea9e53d294d3a45a584d
chenjinjin <chenjinjin@blueocn.com.cn>
2015-05-13 09:56:22 +0800

    conAnaly

--- src/js/graph/graph3.js
+++ src/js/graph/graph3.js
@@ -77,16 +77,22 @@ $(function(){
 
     $('.card-header>ul>li').click(function  () {
         setTimeout(function  () {
-                           pageLoad();
-                       },500);
+           pageLoad();
+       },500);
     });
 
     $('.dd-handle,.trans-name,.trace-name,.tab_link.trace-name-in-transaction,.nav-tabs>li').click(function () {
-        
         setTimeout(function  () {
-                    pageLoad();
-                },100);
+            pageLoad();
+        },100);
     });
+    $('#btn-draw').click(function () {
+        if ($('#form-group2 .select2-chosen').html()=="APP版本") {
+            setTimeout(function  () {
+               pageLoad();
+            },100);
+        }; 
+   });
 
 
 });
